c3d9c7fef997597e8ac93dd09223cf4028a0ad40
Athul Suresh <atulsuresh123@gmail.com>
2017-11-11 18:40:00 +0530

    comonent more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ScreenSelector from './Components/ScreenSelector/ScreenSelector'
+import HeaderNavBar from './Components/HeaderNavBar/HeaderNavBar'
 import {CardGroup} from 'reactstrap'
 import logo from './logo.svg';
 import './App.css';
@@ -8,14 +9,49 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HeaderNavBar />
+        <div className="container">
+          <div className="justify-content-sm-center row">
+            <div className="col col-sm-8">
+              <h2>Pick 5 of your Favorite Options</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+          </div>
         
+        
+
+        <div className="row justify-content-sm-center">
+          <div className="col col-md-3">
+            <div className="screenShotImageContainer">
+              <ScreenSelector />
+            </div>
+          </div>
+
+          <div className="col col-md-3">
+            <div className="screenShotImageContainer">
+              <ScreenSelector />
+            </div>
+          </div>
+
+          <div className="col col-md-3">
+            <div className="screenShotImageContainer">
+              <ScreenSelector />
+            </div>
+          </div>
+
+          <div className="col col-md-3">
+            <div className="screenShotImageContainer">
+              <ScreenSelector />
+            </div>
+          </div>
+
+          <div className="col col-md-3">
+            <div className="screenShotImageContainer">
+              <ScreenSelector />
+            </div>
+          </div>
+        </div>
+        </div>
         <CardGroup className="suggestScreen">
           <ScreenSelector />
           <ScreenSelector />
--- src/Components/ScreenSelector/ScreenSelector.js
+++ src/Components/ScreenSelector/ScreenSelector.js
@@ -12,10 +12,10 @@ export default class ScreenSelector extends Component {
             classnames = "ScreenSelectorCard ScreenSelected"
         return(
         <div >
-            <Card onClick={()=> this.setState({selected:true})} className="ScreenSelectorCard" inverse>
+            <Card onClick={()=> this.setState({selected:!this.state.selected})} className={classnames} inverse>
                 <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=480" alt="Card image cap" />
                 {this.state.selected ? <CardImgOverlay>
-                    <h1>Hello</h1>
+                    <h1>Selected</h1>
                 </CardImgOverlay> : null }
             </Card>
         </div>
